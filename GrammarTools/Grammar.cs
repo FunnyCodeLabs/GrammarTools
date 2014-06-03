ae88888b13c71d4730893d83b0e07f71ad7effa0
Andrew Gadion <andrew.gadion@gmail.com>
2014-06-03 20:23:53 +0300

    parsing

--- GrammarTools/Grammar.cs
+++ GrammarTools/Grammar.cs
@@ -11,52 +11,57 @@ namespace GrammarTools
         private Dictionary<string, NonTerminal> __NonTerminals = new Dictionary<string, NonTerminal>();
         private Dictionary<string, Terminal> __Terminals = new Dictionary<string, Terminal>();
         private List<Rule> __Rules = new List<Rule>();
-
+        private readonly Terminal __Epsilon = new Terminal("e");
         private List<NonTerminal> __EpsilonNonterminals = null;
 
         public Grammar()
         {
-
+            __Terminals.Add("e", __Epsilon);
         }
 
         private void AddRule(string line)
         {
             string[] rightLeft = line.Split(new string[] { "->" }, StringSplitOptions.RemoveEmptyEntries);
-            if (rightLeft.Length < 1)
+            if (rightLeft.Length == 2)
                 throw new ApplicationException("Error parsing: [" + line + "]");
 
 
-            string right = rightLeft[0].Trim();
-            if (!Regex.IsMatch(right, nonTermPattern)
+            string left = rightLeft[0].Trim();
+            NonTerminal leftNonTerminal;
+            if (!AddNonTerminal(left, out leftNonTerminal))
                 throw new ApplicationException();
 
-            if (rightLeft.Length == 1)
-            {
+            string right = rightLeft[1].Trim();
+            string[] tokenStrings = right.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
 
-            }
-            else
+            List<IToken> rightTokens = new List<IToken>();
+            foreach (var tokenStr in tokenStrings)
             {
-                string left = rightLeft[1].Trim();
-                string[] tokenStrings = left.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-
-                foreach (var tokenStr in tokenStrings)
+                NonTerminal nonTerm;
+                if (!AddNonTerminal(tokenStr, out nonTerm))
                 {
-                    IToken token;
-                    if (!AddNonTerminal(tokenStr))
-                        AddTerminal(tokenStr);
-
+                    Terminal term;
+                    if (AddTerminal(tokenStr, out term))
+                        rightTokens.Add(term);
+                    else
+                        throw new ApplicationException();
                 }
+                else
+                    rightTokens.Add(nonTerm);
             }
+
+            Rule rule = new Rule(leftNonTerminal, rightTokens);
+            __Rules.Add(rule);
         }
 
-        private bool AddNonTerminal(string nonTermString)
+        private bool AddNonTerminal(string nonTermString, out NonTerminal nonTerminal)
         {
             string nonTermPattern = "[A-Z]";
 
+            nonTerminal = null;
             if (!Regex.IsMatch(nonTermString, nonTermPattern))
                 return false;
 
-            NonTerminal nonTerminal;
             if (__NonTerminals.ContainsKey(nonTermString))
                 nonTerminal = __NonTerminals[nonTermString];
             else
@@ -68,14 +73,15 @@ namespace GrammarTools
             return true;
         }
 
-        private bool AddTerminal(string termString)
+        private bool AddTerminal(string termString, out Terminal terminal)
         {
             string termPattern = "[a-z|+|-|*|/|(|)]";
+
+            terminal = null;
 
             if (!Regex.IsMatch(termPattern, termString))
                 return false;
 
-            Terminal terminal;
             if (__Terminals.ContainsKey(termString))
                 terminal = __Terminals[termString];
             else
--- GrammarTools/Rule.cs
+++ GrammarTools/Rule.cs
@@ -14,6 +14,7 @@ namespace GrammarTools
         public Rule(NonTerminal left, List<IToken> right)
         {
             LeftPart = left;
+            LeftPart.Rule = this;
             RightPart = right;
         }
     }
